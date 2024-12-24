4f7344fba53f3b427ae71f0d5d3aa5931d1ecd73
XNTEABDSC <2364400855@qq.com>
2024-12-24 18:17:31 +0800

    v0.7.0.6

--- _modinfo.lua
+++ _modinfo.lua
@@ -1,7 +1,7 @@
 return {
 	name='Silly Wars',
 	description='https://github.com/XNTEABDSC/Silly-Wars/wiki',
-	version='v0.7.0.4',
+	version='v0.7.0.5',
 	shortname='ZKSW',
 	mutator='1',
 	game='Zero-K',
