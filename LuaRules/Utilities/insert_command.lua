501a9fe063748c50cc9de1eaa247c028e115237e
XNTEABDSC <2364400855@qq.com>
2025-01-30 21:45:06 +0800

    insert_command

--- LuaRules/Utilities/insert_command.lua
+++ LuaRules/Utilities/insert_command.lua
@@ -1,18 +1,19 @@
 
 local spGiveOrderToUnit=Spring.GiveOrderToUnit
 local CMD_INSERT=CMD.INSERT
-function Spring.Utilities.InsertOrderToUnit(unitId,PosOrTag,Pos_Tag,CmdId,params,option)
+local CMD_OPT_ALT=CMD.OPT_ALT
+function Spring.Utilities.InsertOrderToUnit(unitId,PosOrTag,Pos_Tag,cmdID,cmdParam,cmdOpts)
 
-    local insertparams={Pos_Tag,CmdId,option}
+    local insertparams={Pos_Tag,cmdID,cmdOpts}
     ---@type any
     local insertOption=0
 
-    for index, value in ipairs(params) do
+    for index, value in ipairs(cmdParam) do
         insertparams[index+3]=value
     end
     
     if PosOrTag then
-        insertOption={"alt"}
+        insertOption=CMD_OPT_ALT
     end
     spGiveOrderToUnit(
         unitId,
