880981e7464ede2a43e4c3d9a913ea569ec7bb70
XNTEABDSC <2364400855@qq.com>
2025-05-04 21:23:44 +0800

    tiptest: tactic ai copy

--- gamedata/tweak_unit_defs/tiptest.lua
+++ gamedata/tweak_unit_defs/tiptest.lua
@@ -1,5 +1,6 @@
 local unitDef=UnitDefs.tiptest
 unitDef.metalcost=unitDef.metalcost*1.5
+unitDef.customparams.tactical_ai_defs_copy="vehassault"
 VFS.Include("LuaRules/Utilities/to_make_op_things.lua")
 local utils_op=Spring.Utilities.to_make_op_things
 utils_op.MakeAddSillyBuild("tiptest")
