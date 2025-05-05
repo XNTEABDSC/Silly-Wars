1b1bc83cf8e31e2a606a39b98ee6a787327c4bd6
XNTEABDSC <2364400855@qq.com>
2025-05-05 22:36:13 +0800

    temporarily disable nebulanebula

--- units/nebulanebula.lua
+++ units/nebulanebula.lua
@@ -7,6 +7,10 @@ local utils_op=Spring.Utilities.to_make_op_things
 VFS.Include("LuaRules/Utilities/to_make_very_op_things.lua")
 local to_make_very_op_things=Spring.Utilities.to_make_very_op_things
 
+if true then
+	return {}
+end
+
 local uds=utils_op.CopyTweakSillyBuildMorphAuto("nebula","nebulanebula",function (ud)
     ud.name=ud.name .. "s' " .. ud.name
     ud.description = ud.description .. " of " .. ud.description
