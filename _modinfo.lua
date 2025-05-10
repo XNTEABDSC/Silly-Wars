62cf384f31fc61f3f286bd69162f4e3289ae0b1d
XNTEABDSC <2364400855@qq.com>
2025-05-10 22:39:13 +0800

    v0.9.0.15

--- _modinfo.lua
+++ _modinfo.lua
@@ -1,13 +1,13 @@
 return {
 	modtype=1,
-	shortname="ZKSW",
-	game="Zero-K",
-	description="https://github.com/XNTEABDSC/Silly-Wars/wiki",
-	version="v0.9.0.13",
-	name="Silly Wars",
-	depend={
-		[1]="ZK Modders Dream v0.0.0.12",
-	},
 	shortGame="ZK",
+	depend={
+		[1]="ZK Modders Dream v0.0.0.14",
+	},
+	version="v0.9.0.14",
+	name="Silly Wars",
+	shortname="ZKSW",
 	mutator="Silly War",
+	description="https://github.com/XNTEABDSC/Silly-Wars/wiki",
+	game="Zero-K",
 }
