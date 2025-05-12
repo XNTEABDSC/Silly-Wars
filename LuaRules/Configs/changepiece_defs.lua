2269f6155d17aadff570c305bb50e61b0b8e06cd
XNTEABDSC <2364400855@qq.com>
2025-05-12 18:18:22 +0800

    try changepiece

---
+++ LuaRules/Configs/changepiece_defs.lua
@@ -0,0 +1,24 @@
+--unit_draw_defs.lua
+--
+--def format is: unitDefID = {{sourceunitDefID,sourcepieceName,targetpieceName}},
+--
+
+return {
+--	tank = {
+--		{"tanklibrary","turret","turret"}
+--	},
+--[=[
+	stryker =	{
+		{"strykerwheel","strykerwheel","lwheel1"},
+		{"strykerwheel","strykerwheel","lwheel2"},
+		{"strykerwheel","strykerwheel","lwheel3"},
+		{"strykerwheel","strykerwheel","lwheel4"},
+		{"strykerwheel","strykerwheel","rwheel1"},
+		{"strykerwheel","strykerwheel","rwheel2"},
+		{"strykerwheel","strykerwheel","rwheel3"},
+		{"strykerwheel","strykerwheel","rwheel4"},
+		{"strykerlights","strykerlightsfront","frontlights"},
+		{"strykerlights","strykerlightsback","backlights"},
+	},]=]
+	
+}
